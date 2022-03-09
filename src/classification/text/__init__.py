6d765708bcee21964d949039f05d62bcbc3a16c6
jakob1111996 <jakob.kruse@jina.ai>
2022-03-09 11:14:21 +0100

    tests: improve coverage

--- src/classification/text/__init__.py
+++ src/classification/text/__init__.py
@@ -1,5 +1,6 @@
 """Package for text based emotion classifiers"""
 
 from .bert_classifier import BertClassifier  # noqa: F401
+from .distilbert_classifier import DistilBertClassifier  # noqa: F401
 from .nrclex_classifier import NRCLexTextClassifier  # noqa: F401
 from .text_emotion_classifier import TextEmotionClassifier  # noqa: F401
---
+++ tests/classification/text/test_distilbert_classifier.py
@@ -0,0 +1,54 @@
+import os.path
+import shutil
+
+import numpy as np
+import pytest
+
+from src.classification.text import DistilBertClassifier
+from src.data.text_data_reader import Set, TextDataReader
+
+
+def test_distilbert_initialization():
+    classifier = DistilBertClassifier()
+    assert not classifier.classifier
+    assert not classifier.is_trained
+
+    classifier = DistilBertClassifier({"model_name": "bert_123_test"})
+    assert classifier.model_name == "distilbert_en_uncased_L-6_H-768_A-12"
+
+    with pytest.raises(RuntimeError):
+        classifier.classify()
+
+
+def test_distilbert_workflow():
+    classifier = DistilBertClassifier()
+    assert not classifier.classifier
+    train_parameters = {
+        "epochs": 1,
+        "set": Set.TEST,  # Speedup training by using test set which is smaller
+    }
+    classifier.data_reader = TextDataReader(folder="tests/test_data")
+    classifier.data_reader.file_map[Set.TEST] = "text_test.csv"
+    classifier.data_reader.file_map[Set.VAL] = "text_test.csv"
+    classifier.train(train_parameters)
+
+    shutil.rmtree("tests/temp/bert", ignore_errors=True)
+    save_parameters = {"save_path": "tests/temp/bert"}
+    classifier.save(save_parameters)
+    assert os.path.exists("tests/temp/bert")
+    assert os.path.exists("tests/temp/bert/saved_model.pb")
+    results = classifier.classify()
+    assert isinstance(results, np.ndarray)
+    assert results.shape == (30,)
+
+    new_classifier = DistilBertClassifier()
+    new_classifier.load(save_parameters)
+    new_classifier.data_reader = TextDataReader(folder="tests/test_data")
+    new_classifier.data_reader.file_map[Set.TEST] = "text_test.csv"
+    new_results = new_classifier.classify()
+    assert np.array_equal(new_results, results)
+
+    with pytest.raises(RuntimeError):
+        new_classifier.save({"save_path": "tests/temp/bert"})
+
+    shutil.rmtree("tests/temp/bert", ignore_errors=True)
