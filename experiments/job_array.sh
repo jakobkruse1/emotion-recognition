2f0ed5a845f90521f99b040521086f1610781d75
Jakob Kruse <jakob.kruse@jina.ai>
2022-04-22 11:16:52 -0400

    feat: sbatch instead of LLsub

--- experiments/job_array.sh
+++ experiments/job_array.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Slurm sbatch options
+#SBATCH -o experiment.log-%j
+#SBATCH -n 8
+#SBATCH -c 20
+#SBATCH -N 4
+#SBATCH --gres=gpu:volta:1
+#SBATCH --exclusive
+
 source /etc/profile
 module load anaconda/2022a
 module load cuda/11.6
@@ -9,6 +17,4 @@ echo "Number of Tasks: " $LLSUB_SIZE
 
 TFHUB_CACHE_DIR="$(pwd)/models/cache" python experiments/scripts/parameter_text_models.py $LLSUB_RANK $LLSUB_SIZE
 
-# Run this file via LLsub ./experiments/job_array.sh -s cores -g volta:1 [nodes,processes_per_node,threads_per_process]
-# Number of cores controls the RAM - each core gives 8GB of RAM
 # Might need to run chmod u+x job_array.sh
