4dbe5bcfe3c54195c1bb6cb74dbe06d796223076
Jakob Kruse <jakob.kruse@jina.ai>
2022-04-22 12:54:17 -0400

    fix: sbatch options

--- experiments/job_array.sh
+++ experiments/job_array.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Slurm sbatch options
-#SBATCH -o experiment.log-%j
-#SBATCH -n 8
-#SBATCH -c 20
-#SBATCH -N 4
-#SBATCH --gres=gpu:volta:1
-#SBATCH --exclusive
-
 source /etc/profile
 module load anaconda/2022a
 module load cuda/11.6
