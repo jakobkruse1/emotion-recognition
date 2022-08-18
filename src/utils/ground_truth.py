5b12e145d4e8f860a502fc903b4ac2af679d4597
Jakob Kruse <jakob.kruse@jina.ai>
2022-08-18 11:11:54 -0400

    chore: print before running

--- src/utils/ground_truth.py
+++ src/utils/ground_truth.py
@@ -59,14 +59,14 @@ def experiment_ground_truth(video_file: str) -> None:
     """
     video = VideoFileClip(video_file)
     port = 6060 + int(os.path.basename(video_file)[:3])
+    print(
+        f"Running face-api.js to get ground truth for experiment {port-6060}."
+    )
     emotions = _get_emotions(video, port)
     destination = os.path.join(
         "data",
         "ground_truth",
         f"{os.path.basename(video_file).split('.')[0]}_emotions.json",
-    )
-    print(
-        f"Running face-api.js to get ground truth for experiment {port-6060}."
     )
     with open(destination, "w") as dest_file:
         json.dump(emotions, dest_file)
