b5c0c43a4bd4b09d434f0afb011f4bcc1c01de3e
jakob1111996 <jakob.kruse@jina.ai>
2022-11-22 12:54:14 +0100

    chore: debug github runner

--- tests/classification/image/test_efficientnet_classifier.py
+++ tests/classification/image/test_efficientnet_classifier.py
@@ -27,29 +27,39 @@ def test_efficientnet_workflow():
         "which_set": Set.TRAIN,
     }
     classifier.data_reader = ImageDataReader(folder="tests/test_data/image")
+    print(1)
     classifier.train(train_parameters)
+    print(2)
     assert len(classifier.model.layers) == 4
-
+    print(3)
     shutil.rmtree("tests/temp/efficient", ignore_errors=True)
+    print(4)
     save_parameters = {"save_path": "tests/temp/efficient"}
     classifier.save(save_parameters)
+    print(5)
     assert os.path.exists("tests/temp/efficient")
     assert os.path.exists("tests/temp/efficient/saved_model.pb")
+    print(6)
     results = classifier.classify()
+    print(7)
     assert isinstance(results, np.ndarray)
     assert results.shape == (7,)
-
+    print(8)
     new_classifier = MultiTaskEfficientNetB2Classifier()
+    print(9)
     new_classifier.load(save_parameters)
+    print(10)
     new_classifier.data_reader = ImageDataReader(
         folder="tests/test_data/image"
     )
+    print(11)
     new_results = new_classifier.classify()
+    print(12)
     assert np.array_equal(new_results, results)
 
     with pytest.raises(RuntimeError):
         new_classifier.save({"save_path": "tests/temp/efficient"})
-
+    print(13)
     shutil.rmtree("tests/temp", ignore_errors=True)
 
 
